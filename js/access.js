e96f2b39490638fbacd234d302bb5627412c123f
awilson64-maker <awilson64@wisc.edu>
2026-06-15 11:20:15 -0500

    Cap default Census Gap Lens fit zoom to county level

--- js/access.js
+++ js/access.js
@@ -21,3 +21,37 @@ window.ConveneAccess = (() => {
 
   return { activeCountyId, setActiveCountyId, allowedCountyId };
 })();
+
+(() => {
+  if (!window.L || window.__conveneCensusFitPatchLoaded) return;
+  window.__conveneCensusFitPatchLoaded = true;
+
+  const originalMapFactory = L.map;
+
+  L.map = function conveneCensusFitMapFactory() {
+    const map = originalMapFactory.apply(this, arguments);
+    const rawTarget = arguments[0];
+    const targetId = typeof rawTarget === 'string' ? rawTarget : rawTarget?.id;
+
+    if (targetId === 'censusMap' && !map.__conveneCensusFitPatched) {
+      map.__conveneCensusFitPatched = true;
+      const originalFitBounds = map.fitBounds;
+
+      map.fitBounds = function conveneCensusFitBounds(bounds, options = {}) {
+        const nextOptions = { ...(options || {}) };
+        if (nextOptions.maxZoom == null) {
+          const county = activeCounty();
+          nextOptions.maxZoom = Number(county?.censusFitMaxZoom ?? county?.mapZoom ?? 10);
+        }
+        return originalFitBounds.call(this, bounds, nextOptions);
+      };
+    }
+
+    return map;
+  };
+
+  function activeCounty() {
+    const selected = document.getElementById('countySelect')?.value || window.CONVENE_DEFAULT_COUNTY;
+    return window.CONVENE_COUNTIES?.[selected] || window.CONVENE_COUNTIES?.[window.CONVENE_DEFAULT_COUNTY];
+  }
+})();
